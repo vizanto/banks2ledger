5d455ad29b7493d45a8977e7798beb313704671e
Danny Wilson <danny@prime.vc>
2017-08-03 22:55:15 +0200

    Pad and align Beancount transaction lines to 80-chars wide

--- src/banks2ledger/core.clj
+++ src/banks2ledger/core.clj
@@ -384,15 +384,17 @@
 (defn print-beancount-entry [acc-maps
                              {:keys [date flag payee descr reference metas postings]}]
   (printf "\n%s %s \"%s\" \"%s\"" date (or flag "*") (str payee) (str descr))
-  (when-not (empty? reference) (printf " ^%s" reference))
+  (when-not (empty? reference)
+    (printf (str" %" (- 60 (count payee) (count descr)) "s")
+            (str "^" reference)))
   (println)
   (doseq [[k v] metas]
-    (printf "  %-58s %24s\n" (str k ":") v)) ; conversion-fee: 3 EUR, etc
+    (printf (str "  %s %" (- 75 (count k)) "s\n") (str k ":") v)) ; conversion-fee: 3 EUR, etc
   (doseq [{:keys [amount currency account commented]}
           (decide-all-accounts acc-maps (or payee descr "") postings)]
-    (printf (if-not commented "  %-58s" #_else " ;%-58s") account)
+    (printf (if-not commented "  %s" #_else " ;%s") account)
     (when amount
-      (printf " %20s" amount)
+      (printf (str " %" (- 72 (count account)) "s") amount)
       (when currency (printf " %s" currency)))
     (println)))
 
